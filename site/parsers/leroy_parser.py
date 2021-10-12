9a11d51b46682c5636eb700216d04166476e249d
Your Name <tarskaya.alina@mail.ru>
2021-10-12 23:07:53 +0300

    Added parser example

---
+++ site/parsers/leroy_parser.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+from site.parsers.utils import parse_category
+
+
+def get_data(depth=[0, 0, 0, 0]):
+    result = []
+
+    s = requests.Session()
+    categories_data = s.get('https://leroymerlin.ru/content/elbrus/moscow/ru/displayedCatalogue.json')
+    to_parse = json.loads(categories_data.text)
+
+    if depth[0] == 0:
+        depth[0] = len(to_parse)
+    for cat in to_parse[:depth[0]]:
+        result.append(parse_category(cat, depth[1:]))
+
+    return result
+
